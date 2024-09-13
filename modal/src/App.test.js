159fea0dde2486710e34c540b595c388f013019c
Esra Sarıgül <esra.sarigul14@gmail.com>
2024-09-13 11:19:50 +0300

    updated app.test for fetchData error handling

--- modal/src/App.test.js
+++ modal/src/App.test.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { cleanup, fireEvent, render, screen, waitFor } from "@testing-library/react";
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import App from "./App";
 import { getReportIssueType } from "./api/requests";
+import { Texts } from "./text/tr";
 
-jest.mock('./api/requests', () => ({
+jest.mock("./api/requests", () => ({
   getReportIssueType: jest.fn(),
 }));
 const mockResponseData = [
@@ -18,19 +25,35 @@ describe("App Component", () => {
 
   test("renders without crashing", () => {
     render(<App />);
-    expect(screen.getByText(/Open Modal/i)).toBeInTheDocument();
+    expect(screen.getByText(Texts.open_modal)).toBeInTheDocument();
   });
+
   it("does not render when show is false", () => {
     render(<App />);
     expect(screen.queryByTestId("modal")).not.toBeInTheDocument();
   });
-  it("modal open when click button", async() => {
+
+  it("modal open when click button", async () => {
     getReportIssueType.mockResolvedValueOnce(mockResponseData);
     render(<App />);
-    const button = screen.getByText(/Open Modal/i);
+    const button = screen.getByText(Texts.open_modal);
     fireEvent.click(button);
     await waitFor(() => {
-      expect(screen.queryByTestId('modal')).toBeInTheDocument();
-    });  });
+      expect(screen.queryByTestId("modal")).toBeInTheDocument();
+    });
+  });
 
+  it("should handle error correctly and stop loading when getReportIssueType fails", async () => {
+    const mockError = new Error("API error");
+    const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
+    getReportIssueType.mockRejectedValueOnce(mockError);
+    render(<App />);
+    const button = screen.getByText(Texts.open_modal);
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(getReportIssueType).toHaveBeenCalledTimes(1); // API çağrısı yapıldı
+      expect(consoleSpy).toHaveBeenCalledWith('fetchData error:', mockError); // Hata loglandı
+    });
+  });
 });
